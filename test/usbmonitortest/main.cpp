93a3d2b3861bd97ea73e566fcff1578c44ee6fd1
Li-Yaosong <liyaosong1@qq.com>
2025-09-10 01:19:38 +0800

    添加 USB 设备移除和更改事件处理程序
    在 usbmonitortest main.cpp 中添加了 usbDeviceRemoved 和 usbDeviceChanged 事件的信号处理程序。此外，还更新了 QTimer::singleShot，使其使用 10 秒退出测试程序，并将 qApp 设置为所有 lambda 连接的父级。

--- test/usbmonitortest/main.cpp
+++ test/usbmonitortest/main.cpp
@@ -7,11 +7,16 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     UsbMonitor::startMonitor();
     QObject::connect(qApp, &QCoreApplication::aboutToQuit, UsbMonitor::instance(), &UsbMonitor::destroy);
-    QObject::connect(UsbMonitor::instance(), &UsbMonitor::usbDeviceAdded, [](const QString &deviceDescription) {
+    QObject::connect(UsbMonitor::instance(), &UsbMonitor::usbDeviceAdded, qApp, [](const QString &deviceDescription) {
         qDebug() << "USB device added:" << deviceDescription;
     });
-
-    QTimer::singleShot(5000, [] {
+    QObject::connect(UsbMonitor::instance(), &UsbMonitor::usbDeviceRemoved, qApp, [](const QString &deviceDescription) {
+        qDebug() << "USB device removed:" << deviceDescription;
+    });
+    QObject::connect(UsbMonitor::instance(), &UsbMonitor::usbDeviceChanged, qApp, [](const QString &deviceDescription) {
+        qDebug() << "USB device changed:" << deviceDescription;
+    });
+    QTimer::singleShot(10000, qApp, [] {
         QCoreApplication::quit();
     });
     return a.exec();
